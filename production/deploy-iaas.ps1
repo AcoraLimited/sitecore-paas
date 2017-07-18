231d4f867ccec3aa299716c1d3b16a9cf2d5df6e
Craig Hurt <churt@acora.com>
2017-07-18 09:25:38 +0100

    Updated IaaS netsted templates for testing.

---
+++ production/deploy-iaas.ps1
@@ -0,0 +1,13 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+    [string] $ResourceGroupLocation = "West Europe",
+    [string] $TemplateFile = ".\azuredeploy-iaas.json",
+    [string] [Parameter(Mandatory=$true)] $KeyVaultName,
+    [string] [Parameter(Mandatory=$true)] $KeyVaultResourceGroupName
+)
+
+#Login-AzureRmAccount
+#Select-AzureRmSubscription -SubscriptionName "TODO"
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation;
+New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -keyVaultName $KeyVaultName -keyVaultResourceGroupName $KeyVaultResourceGroupName;
